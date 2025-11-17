56cbb57d598a3a74889b89488bfead803a9664e3
Jamir Banks <jrule7220@gmail.com>
2025-11-17 17:01:07 -0500

    Update main.py

--- agent/main.py
+++ agent/main.py
@@ -1,1 +1,29 @@
+import time
+import yaml
+from collector import collect_logs
+from normalizer import normalize
+from forwarder import send_to_siem
+from watcher import start_realtime_monitor
+from cli import cli
+
+def load_config():
+    with open("config.yaml", "r") as f:
+        return yaml.safe_load(f)
+
+def run_agent():
+    config = load_config()
+    args = cli()
+
+    if args.realtime or config["use_realtime_monitoring"]:
+        return start_realtime_monitor(config)
+
+    print("[*] Mini-SOC Agent started (polling mode)")
+    while True:
+        logs = collect_logs(config)
+        normalized = normalize(logs)
+        send_to_siem(normalized, config)
+        time.sleep(config["interval"])
+
+if __name__ == "__main__":
+    run_agent()
 
